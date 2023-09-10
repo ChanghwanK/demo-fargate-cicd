ca5e649bfbb49738afc746aa75e1a8d2d99f7917
devbloo <zpswl45@naver.com>
2023-09-10 15:06:51 +0900

    init

---
+++ src/main/kotlin/com/example/greeting/GreetingApiController.kt
@@ -0,0 +1,13 @@
+package com.example.greeting
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GreetingApiController {
+
+    @GetMapping("/api/greeting")
+    fun greeting(): String {
+        return "Hello, World!"
+    }
+}
